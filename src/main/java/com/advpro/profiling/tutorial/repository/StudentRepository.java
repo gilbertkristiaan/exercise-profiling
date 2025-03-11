73f4bc3cef467f65a7b1bf1cd448c4530fa13739
gilbertkristiaan <gilbertkristian79@gmail.com>
2025-03-12 00:03:06 +0700

    [REFACTOR] Optimizing method findStudentWithHighestGpa

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -3,10 +3,13 @@ package com.advpro.profiling.tutorial.repository;
 import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value = "SELECT * FROM STUDENTS ORDER BY gpa DESC LIMIT 1", nativeQuery = true)
+    Optional<Student> findFirstByOrderByGpaDesc();
 }
